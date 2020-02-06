e016757e0535ff6f5816251a5d0454962a2eec19
Rhodine <rorleans_lindsay@yahoo.co.uk>
2020-02-06 11:54:14 +0000

    deleting posts - blocked

--- controllers/posts.js
+++ controllers/posts.js
@@ -18,7 +18,23 @@ var PostsController = {
 
       res.status(201).redirect('/posts');
     });
+  },
+
+  Delete: function(req, res){
+    var post = new Post
+console.log("**1**");
+console.log({_id: req.params.post.id});
+    post.remove({_id: req.params.id}, function(err) {
+      console.log("***2**");
+      if(err) {
+        res.json({status: false, error: "Deleting post is not successfull"});
+        return;
+      }
+    });
+    console.log("Hi");
+    res.status(201).redirect('/posts');
   }
 };
+
 
 module.exports = PostsController;
--- models/post.js
+++ models/post.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
+  id: String,
   message: String,
+  date: { type: Date, default: Date.now() },
 });
 
 var Post = mongoose.model('Post', PostSchema);

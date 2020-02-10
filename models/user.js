64afaa227e085ad748d2bf71bbfc08ba50822e5d
JCWBiles <jcbiles@outlook.com>
2020-02-10 14:09:28 +0000

    added image and played with css

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
+var bcrypt = require ('bcrypt');
 
 var UserSchema = new mongoose.Schema({
   user_id: String,
-  id: String,
+  _id: String,
   firstname: String,
   lastname: String,
   email: String,
@@ -10,11 +11,23 @@ var UserSchema = new mongoose.Schema({
   dateCreated: { type: Date, default: Date.now() },
 });
 
+UserSchema.pre('save', function(next){
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash){
+    if(err){
+      return next(err);
+    }
+    user.password = hash;
+  next();
+  })
+});
+
+UserSchema.set('toJSON', { virtuals: true });
+
 var User = mongoose.model('Users', UserSchema);
 // 
 // User.findByIdAndRemove({'_id': {"$oid":"5e3bfb927b5e1231762bd7f8"}}).exec(function(err){
 //   console.log(User);
 // });
-
 
 module.exports = User;
